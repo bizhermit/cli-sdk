769642920c5e39e4cc037ffefe04a459a4129186
bizhermit <rsenda@bizhermit.com>
2022-02-07 20:08:25 +0900

    write line, read line

--- src/dist/index.ts
+++ src/dist/index.ts
@@ -1,5 +1,19 @@
-const main = () => {
-  console.log("call main.");
+export const rl = (message?: string) => {
+    return new Promise<string>((resolve) => {
+        if (message) process.stdout.write(message);
+        process.stdin.resume().on("data", (data) => {
+            process.stdin.pause();
+            resolve(data.toString().trim());
+        });
+    });
 };
 
-export default main;
+export const wl = (message?: string) => {
+    if (message) process.stdout.write(message + "\n");
+};
+
+const CliSdk = {
+    rl,
+    wl,
+};
+export default CliSdk;
--- test.js
+++ test.js
@@ -1,1 +1,14 @@
 console.log("--- test ---");
+
+const CliSdk = require("./dist/index.js").default;
+
+(async () => {
+    const inputs = [];
+    inputs.push(await CliSdk.rl("input1 > "));
+    inputs.push(await CliSdk.rl("input2 > "));
+    inputs.push(await CliSdk.rl("input3 > "));
+    console.log(inputs);
+    for await (const text of inputs) {
+        CliSdk.wl(text);
+    }
+})();
